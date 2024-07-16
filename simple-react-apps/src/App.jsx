a93fa8412363c3af5718137e81dc4f00a07d8c9c
Gabriel <gsantandreusantonja@gmail.com>
2024-07-16 14:58:08 +0200

    Aplicación simple - stopwatch (curso terminado)

--- simple-react-apps/src/App.jsx
+++ simple-react-apps/src/App.jsx
@@ -2,6 +2,7 @@ import Counter from './Counter.jsx';
 import ColorPicker from './ColorPicker.jsx';
 import ToDoList from './ToDoList.jsx';
 import DigitalClock from './DigitalClock.jsx';
+import Stopwatch from './Stopwatch.jsx';
 
 function App() {
 
@@ -17,6 +18,9 @@ function App() {
             <br /><br />
             <hr /> 
             <DigitalClock></DigitalClock>
+            <br /><br />
+            <hr /> 
+            <Stopwatch></Stopwatch>
         </>
     );
 }
